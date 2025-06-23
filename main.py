35e4bc2d5b260f26b4d058c06d0ecc34cd49dd8e
david <dmsim159@gmail.com>
2025-06-22 20:23:33 -0700

    Create a function to convert currencies

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ import requests
 API_KEY = 'fca_live_EjHM9RZHysvODRCpPg0jIcPdT5r5AS4KF0KkyFqf'
 BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 
+CURRENCIES = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD']
 
-    
+def convert_currency(base):
+        currencies = ",".join(CURRENCIES)
+        url = f'{BASE_URL}&base_currency={base}&currencies={currencies}'
+        try:
+            response = requests.get(url)
+            data = response.json()
+            print(data)
+            return data
+        except Exception as e:
+            print(e)
+            return None
+      
