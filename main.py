8157a1942d465aad613aaaabd5842ba020da6355
david <dmsim159@gmail.com>
2025-06-22 20:16:00 -0700

    Assign API key and Base Url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from currency-converter!")
+import requests
+
+API_KEY = 'fca_live_EjHM9RZHysvODRCpPg0jIcPdT5r5AS4KF0KkyFqf'
+BASE_URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 
 
-if __name__ == "__main__":
-    main()
+    
